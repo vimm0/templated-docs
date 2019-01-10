7407f83048df8341f87f512a860b025ea25f371f
vimm0 <vimmrana0@gmail.com>
2019-01-11 00:25:06 +0545

    fix for django2

--- templated_docs/__init__.py
+++ templated_docs/__init__.py
@@ -70,7 +70,8 @@ def find_template_file(template_name):
     load a template in memory, because we'll deal with it ourselves.
     """
     for loader in _get_template_loaders():
-        for origin in loader.get_template_sources(template_name, None):
+        # get_template_sources() takes 2 positional arguments but 3 were given (Django2)
+        for origin in loader.get_template_sources(template_name):
             path = getattr(origin, 'name', origin)  # Django <1.9 compatibility
             if os.path.exists(path):
                 return path
